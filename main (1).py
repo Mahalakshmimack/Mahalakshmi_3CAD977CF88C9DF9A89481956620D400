4f0ff2ca9b7c730c008c71d3285722e44b2731ae
Mahalakshmimack <144592208+Mahalakshmimack@users.noreply.github.com>
2023-10-25 12:48:59 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,6 +1,21 @@
-# Write a python program  that determines whether a year entered  by the user  is a leapyear or not using ifelif-else statements
-year=int(input("Enter year to be checked:"))
-if(year%4==0 and year%100!=0 or year%400==0):
-    print("The year is a leap year!")
-else:
-    print("The year isn't a leap year!")
+class student:
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
+
+def sort_student(student_list):
+  sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+  return sorted_students
+
+students = [
+    student("Amith", "02", 9.8),
+    student("Monesh", "10", 8.5),
+    student("Parvesh", "15", 7.5),
+    student("Rajasekar", "18", 7.4),
+]
+sorted_students = sort_student(students)
+for student in sorted_students:
+  print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
+                                                      student.roll_number,
+                                                      student.cgpa))
